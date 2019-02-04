444992674af6d4278884f7ba57d93e7e6ffa6f1f
yuki kojima <yuki.kojima@nijibox.co.jp>
2019-02-04 22:15:47 +0900

    change :: 社員一覧のスタイル追加、ページャーのボタンのスタイル変更

--- src/components/Memberlist.js
+++ src/components/Memberlist.js
@@ -8,18 +8,18 @@ class Memberlist extends Component {
       let userInfo;
       let currentPage;
       let totalPages;
-      let flgForward = true;
-      let flgBackward = true;
+      let flgNext = true;
+      let flgPrev = true;
       if (this.props.userList){
           userInfo = this.props.userList.item_list;
           currentPage = this.props.userList.summary.current_page;
           totalPages = this.props.userList.summary.total_pages;
 
           if (currentPage === 1) {
-              flgBackward = false;
+              flgPrev = false;
           }
           if (currentPage === totalPages) {
-              flgForward = false;
+              flgNext = false;
           }
         }
     return (
@@ -28,22 +28,23 @@ class Memberlist extends Component {
         {this.props.userList ? <div>
             <h2>社員一覧</h2>
             <div>
-                <ul>
+                <ul className="l-memberlist">
                     {userInfo.map((row, index) => {
                         return (
                           <li key={row.user_id}>
-                            <Link to={"/user/" + row.user_id}>
-                                {row.user_name}
+                            <Link to={"/user/" + row.user_id} className="memberlist">
+                                    <div className="memberlist__img"><img src={row.photo_url} alt={row.user_name + 'の写真'}></img></div>
+                            <div className="memberlist__name">{row.user_name}</div>
                             </Link>
                           </li>
                         );
                     })}
                 </ul>
-                    {flgBackward && <div onClick={e => this.props.loadUserInfo(e)} data-page={parseInt(currentPage, 10) - 1} data-id={this.props.departmentID} data-query={this.props.query}> 前へ</div>
-                }
-                    {flgForward && <div onClick={e => this.props.loadUserInfo(e)} data-page={parseInt(currentPage, 10) + 1} data-id={this.props.departmentID} data-query={this.props.query}>次へ</div>
-                }
-                <div>{currentPage}/{totalPages}ページ</div>
+                <div className="pager">
+                    <button type="button" className={"btn-pager" + ' ' + (!flgPrev ? "is-hidden" : '')} onClick={e => this.props.loadUserInfo(e)} data-page={parseInt(currentPage, 10) - 1} data-id={this.props.departmentID} data-query={this.props.query}> 前へ</button>
+                    <div className="pager__page">{currentPage}/{totalPages}ページ</div>
+                    <button type="button" className={"btn-pager" + ' ' + (!flgNext ? "is-hidden" : '')} onClick={e => this.props.loadUserInfo(e)} data-page={parseInt(currentPage, 10) + 1} data-id={this.props.departmentID} data-query={this.props.query}>次へ</button>
+                </div>
             </div>
         </div> : <p>検索してください</p>
         }
