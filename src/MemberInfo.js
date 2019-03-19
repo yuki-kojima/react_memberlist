e064bf3453742e41e45a7d80c834c91524005c56
yuki kojima <yuki.kojima@nijibox.co.jp>
2019-03-19 16:38:12 +0900

    fix ::  MemberList インデント調整

--- src/MemberInfo.js
+++ src/MemberInfo.js
@@ -19,14 +19,14 @@ class MemberInfo extends Component {
 
     this.loadUserDetail();
   }
-    commonResponseHandling(res) {
-        console.debug(res);
-        if (res.data.code !== "200") {
-            console.error(res.data.data);
-            return Promise.reject("API Error:" + res.data.data.message);
-        }
-        return Promise.resolve(res.data.data);
-    }
+  commonResponseHandling(res) {
+      console.debug(res);
+      if (res.data.code !== "200") {
+          console.error(res.data.data);
+          return Promise.reject("API Error:" + res.data.data.message);
+      }
+      return Promise.resolve(res.data.data);
+  }
 
   loadUserDetail() {
     const { params } = this.props.match;
