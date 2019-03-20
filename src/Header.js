3de99c3dc2337236deca24eae59de232b770c5b0
yuki kojima <yuki.kojima@nijibox.co.jp>
2019-03-20 11:52:35 +0900

    change :: Header ロゴを追加　スタイル修正

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "./img/logo.png";
 
 const Header = () => {
         return (
             <header id="header">
                 <div className="header__inner">
-                    <div className="header__logo"><img src="" width="" heigth="" alt ="NIJIBOX"></img></div>
+                    <h1 className="pagetitle">
+                        <img src={logo} width="45" heigth="45" alt="NIJIBOX"></img>
+                        <span className="pagetitle__text">ニジボックス社員名簿</span>
+                    </h1>
                     <nav className="header__nav">
                         <ul className="nav">
                             <li><div><Link to="/Search">社員名簿</Link></div></li>
                             <li><div><Link to="/Game">神経衰弱</Link></div></li>
-                            <li><div><Link to="/">トップ</Link></div></li>                        
+                            <li><div><Link to="/">トップ</Link></div></li>
                         </ul>
                     </nav>
                 </div>
