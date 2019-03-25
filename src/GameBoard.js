651e1f4a9425d40fb65fb1383164431644c9b0f6
yuki kojima <yuki.kojima@nijibox.co.jp>
2019-03-25 18:02:43 +0900

    change :: GameBoard returnのだし分け方法を変更

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -4,22 +4,20 @@ import './App.css';
 class GameBoard extends Component {
     render() {
         const cardList = this.props.cardList;
-
-        if (cardList.length === 0) {
-            return (
-              <div className="l-game-text">
-                <div className="game-text">部署を選んでください</div>
-              </div>
-            );
-        } else {
-            return (
-                <ul id="card">
-                    {cardList.map((item, index) =>  {
-                        return (<li key={index + item.user_id} data-id={item.user_id} data-img={item.photo_url} onClick={e => this.props.handleCardClick(e)}><img src="./nblogo.jpg" alt="" /></li>);
-                    })}
-                </ul>
-            );
-        }
+        return(
+            <div id="gameBoard">
+                {cardList.length === 0 ?
+                    <div className="l-game-text">
+                        <div className="game-text">部署を選んでください</div>
+                    </div>:
+                    <ul id="card">
+                        {cardList.map((item, index) => {
+                            return (<li key={index + item.user_id} data-id={item.user_id} data-img={item.photo_url} onClick={e => this.props.handleCardClick(e)}><img src="./nblogo.jpg" alt="" /></li>);
+                        })}
+                    </ul>
+                }
+            </div>
+        )
     }
 }
 
