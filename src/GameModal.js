ffc13ef7fdd14f3ec197cec409a09d54411db273
yuki kojima <yuki.kojima@nijibox.co.jp>
2019-03-26 17:47:44 +0900

    add :: ゲームクリア時用のモーダル実装

---
+++ src/GameModal.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+const GameModal = (props) => {
+    console.log(props);
+    return (
+    <div className="modal">
+        <div className="modal__overlay">
+            <div className="modal__content">
+                <div className="modal__header">コンプリート！</div>
+                <div className="modal__body">
+                    <button onClick={e => props.restartGame(e)}>もう一度遊ぶ</button>
+                    <button onClick={e => props.resetGame(e)}>別の部署で遊ぶ</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+
+};
+
+export default GameModal;
