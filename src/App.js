bd7b574dbb8c1e34e48712409e0d5bca5a6263e1
yuki kojima <yuki.kojima@nijibox.co.jp>
2019-03-26 18:50:07 +0900

    fix :: リロードすると404エラーになるためhashRouterに変更

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import axiosCreate from "./utility/axiosCreate";
 import handleResponse from "./utility/handleResponse";
 import 'ress';
