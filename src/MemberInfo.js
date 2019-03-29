78216005cbbf068802c6cf626bcbc0c93e88b1d4
yuki kojima <yuki.kojima@nijibox.co.jp>
2019-03-29 19:33:39 +0900

    change :: MemberInfo スタイル修正

--- src/MemberInfo.js
+++ src/MemberInfo.js
@@ -34,51 +34,77 @@ class MemberInfo extends Component {
   }
   render() {
     return (
-      <div>
-        {this.state.userInfo ? <div>
-            <h2>詳細</h2>
+      <div className="l-memberinfo">
+        {this.state.userInfo ? (
+          <React.Fragment>
             <div className="memberinfo">
-                <div className="memberinfo__img"><img src={this.state.userInfo.main_photo_url} alt={this.state.userInfo.user_name + "の写真"}/></div>
-                <div className="memberinfo__info">
-                  <div className="memberinfo__info__header">
-                    <h3 className="memberinfo-title">
-                      <span className="memberinfo-title__name">{this.state.userInfo.user_name}</span>
-                      <span className="memberinfo-title__sub">{this.state.userInfo.user_kana}</span>
-                    </h3>
-                  </div>
-                  <div className="memberinfo__info__body">
-                    <table className="infotable">
-                      <tbody>
-                        <tr>
-                          <td className="infotable__header">ニックネーム：</td>
-                          <td className="infotable__body">{this.state.userInfo.nickname}</td>
-                        </tr>
-                        <tr>
-                          <td className="infotable__header">部署：</td>
-                          <td className="infotable__body">{this.state.userInfo.department_name}</td>
-                        </tr>
-                        <tr>
-                          <td className="infotable__header">入社日：</td>
-                          <td className="infotable__body">{this.state.userInfo.enter_date}</td>
-                        </tr>
-                        <tr>
-                          <td className="infotable__header">slack名：</td>
-                          <td className="infotable__body">{this.state.userInfo.slack_desplay_name}</td>
-                        </tr>
-                        <tr>
-                          <td className="infotable__header">メールアドレス：</td>
-                          <td className="infotable__body">{this.state.userInfo.mail}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
+              <div className="memberinfo__img">
+                <img
+                  src={this.state.userInfo.main_photo_url}
+                  alt={this.state.userInfo.user_name + "の写真"}
+                />
+              </div>
+              <div className="memberinfo__info">
+                <div className="memberinfo__info__header">
+                  <h3 className="memberinfo-title">
+                    <span className="memberinfo-title__name">
+                      {this.state.userInfo.user_name}
+                    </span>
+                    <span className="memberinfo-title__sub">
+                      {this.state.userInfo.user_kana}
+                    </span>
+                  </h3>
                 </div>
+                <div className="memberinfo__info__body">
+                  <table className="infotable">
+                    <tbody>
+                      <tr>
+                        <td className="infotable__header">
+                          ニックネーム：
+                        </td>
+                        <td className="infotable__body">
+                          {this.state.userInfo.nickname}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="infotable__header">部署：</td>
+                        <td className="infotable__body">
+                          {this.state.userInfo.department_name}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="infotable__header">入社日：</td>
+                        <td className="infotable__body">
+                          {this.state.userInfo.enter_date}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="infotable__header">slack名：</td>
+                        <td className="infotable__body">
+                          {this.state.userInfo.slack_desplay_name}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="infotable__header">
+                          メールアドレス：
+                        </td>
+                        <td className="infotable__body">
+                          {this.state.userInfo.mail}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
-        </div> : <p>該当メンバーがいません</p>
-        }
-        <div className="pagenav">
-        <Link to="/search">検索へ戻る</Link>
-        <Link to="/">トップへ戻る</Link>
+          </React.Fragment>
+        ) : (
+          <p>該当メンバーがいません</p>
+        )}
+        <div className="l-gotop">
+          <div className="gotop">
+            <Link to="/">検索へ戻る</Link>
+          </div>
         </div>
       </div>
     );
